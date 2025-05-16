b2727ebe5255fea27804bc481f849f3d104a0f19
manuella <rm95218@estudante.fieb.edu.br>
2025-05-16 14:47:38 +0000

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import {Routs, Route, Routes} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Home from './Home'
 const App = () => {
   
   return (
     <Routes>
-      <Route path"/" element={<Home/>} />
+      <Route path="/" element={<Home/>} />
     </Routes>
   )
 }
